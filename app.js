a405b79da81d254a49dd617b525f1577cff22727
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-18 20:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const {MONGOURI} = require('./config/keys')
 
 
@@ -34,6 +34,6 @@ if(process.env.NODE_ENV=="production"){
     })
 }
 
-app.listen(PORT,()=>{
-    console.log("server is running on",PORT)
+app.listen(port,()=>{
+    console.log("server is running on",port)
 })
