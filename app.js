2f0a30ca604e8b3ce8593fe676c5145806c920ec
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-18 19:35:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/key')
+const {MONGOURI} = require('./config/keys')
 
 
-mongoose.set('strictQuery', true)
-mongoose.connect(MONGOURI)
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
 
-
+})
 mongoose.connection.on('connected',()=>{
-    console.log("Connected to mongo")
+    console.log("conneted to mongo yeahh")
 })
-
-
 mongoose.connection.on('error',(err)=>{
-    console.log("Error connecting to mongo",err)
+    console.log("err connecting",err)
 })
-
 
 require('./models/user')
 require('./models/post')
@@ -26,6 +24,7 @@ app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
+
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
@@ -36,5 +35,5 @@ if(process.env.NODE_ENV=="production"){
 }
 
 app.listen(PORT,()=>{
-    console.log("server is running on port",PORT)
+    console.log("server is running on",PORT)
 })
