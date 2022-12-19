13719fdda6ed8ce2b6215a496799515d16c06722
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-19 10:09:56 +0530

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,12 +1,8 @@
-//if(process.env.NODE_ENV==='production'){
-  //  module.exports = require('./prod')
-//else{
-//module.exports = require('./dev')
-//}
-
-module.exports={
-    MONGOURI :"mongodb+srv://Prasad:3bJKGA8XMiV55rcv@cluster0.dkplkkf.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"hfauydhajhwg4"
-
+if(process.env.NODE_ENV==='production'){
+    module.exports = require('./prod')
+else{
+module.exports = require('./dev')
 }
+
+
 
