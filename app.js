5796f2a4430dfc1d1b40a26febb883115523b73e
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-19 09:06:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const port = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const {MONGOURI} = require('./config/key')
 
 
 mongoose.connect(MONGOURI,{
