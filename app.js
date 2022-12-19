aba81f82492843d398da0efee510670cd53cec5d
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-19 10:09:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const port = process.env.PORT || 3000;
-//const {MONGOURI} = require('./config/key')
+const {MONGOURI} = require('./config/key')
 
 
 mongoose.connect(MONGOURI,{
@@ -26,13 +26,13 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 
-//if(process.env.NODE_ENV=="production"){
-   // app.use(express.static('client/build'))
-   // const path = require('path')
-   // app.get("*",(req,res)=>{
-    //    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  //  })
-//}
+if(process.env.NODE_ENV=="production"){
+   app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
